34bd6b0d11e26c054443509df74feefeb399896d
Alexandre Vessoni <av.vessoni@gmail.com>
2020-05-27 19:48:54 +0100

    Add Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+// BrowserRouter is the router implementation for HTML5 browsers (vs Native).
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+// Styles
+import { Title } from './styles';
+
 // Why const Arrow Function? -> With TypeScript is more simple to
 // declare the type of component
 
 // Dashboard is a Function component
 const Dashboard: React.FC = () => {
-  return <h1> Dashboard </h1>;
+  return <Title> Expore Repositorys on Github </Title>;
 };
 
 export default Dashboard;
