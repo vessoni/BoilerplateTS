6e028d9ce7c8b7211237a3141a0fe11476350fab
Alexandre Vessoni <av.vessoni@gmail.com>
2020-05-27 20:06:37 +0100

    Add Image

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import github from '../assets/github.svg';
 
 export default createGlobalStyle`
   * {
@@ -9,7 +10,22 @@ export default createGlobalStyle`
   }
 
   body{
-    background: #F0F0F5;
+    background: #F0F0F5 url(${github}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button{
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root{
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button{
+    cursor: pointer;
   }
 
 `;
