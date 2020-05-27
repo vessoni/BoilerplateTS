f330afa6bc943930f5eee890ac2189cb8c5b1af6
Alexandre Vessoni <av.vessoni@gmail.com>
2020-05-27 21:16:07 +0100

    Api Configs

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,47 +1,69 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 // Styles
 import { Title, Form, Repositories } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 // Why const Arrow Function? -> With TypeScript is more simple to
 // declare the type of component
 
 // Dashboard is a Function component
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  // FormEvent is the submit event;
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
-      <Title> Exporer Repositorys on Github </Title>
+      <Title> Git Explore Respository </Title>
 
-      <Form action="">
-        <input placeholder="Type repository name" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Type repository name"
+        />
         <button type="submit"> Search </button>
       </Form>
 
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/4910383?s=460&u=83c4a27c7b7242163c6568a7e0535519ecde2eba&v=4"
-            alt="Alex"
-          />
-          <div>
-            <strong>alexandre/oi</strong>
-            <p> ASDKOASKDOASKOKOASDKAD</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="test">
-          <img
-            src="https://avatars3.githubusercontent.com/u/4910383?s=460&u=83c4a27c7b7242163c6568a7e0535519ecde2eba&v=4"
-            alt="Alex"
-          />
-          <div>
-            <strong>alexandre/oi</strong>
-            <p> ASDKOASKDOASKOKOASDKAD</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="test">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
