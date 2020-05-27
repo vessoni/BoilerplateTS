7b9c02b11dc6c7b04d1f6b6a014b655f0a9fb4bd
Alexandre Vessoni <av.vessoni@gmail.com>
2020-05-27 19:29:02 +0100

    Create Routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Why const Arrow Function? -> With TypeScript is more simple to
+// declare the type of component
+
+// Dashboard is a Function component
+const Dashboard: React.FC = () => {
+  return <h1> Dashboard </h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Why const Arrow Function? -> With TypeScript is more simple to
+// declare the type of component
+
+// Repository is a Function component
+const Repository: React.FC = () => {
+  return <h1> Repository </h1>;
+};
+
+export default Repository;
